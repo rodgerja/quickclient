35095da761683aec8a73228c54084751428121e6
james.rodgers <jcm.rodger@gmail.com>
2019-08-16 12:27:30 +0100

    Configure pom

--- src/main/java/com/bearcorp/quickclient/QuickClient.java
+++ src/main/java/com/bearcorp/quickclient/QuickClient.java
@@ -2,18 +2,22 @@ package com.bearcorp.quickclient;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 
+import com.bearcorp.quickstart.QuickStart;
+
 public class QuickClient {
 
+  private QuickStart quickStart = new QuickStart();
+
   public static void main(String[] args) {
+    QuickClient quickClient = new QuickClient();
+
     String str = randomAlphanumeric(10);
-    System.out.println("I am QuickStart " + str);
+    String msg = "I am QuickClient " + quickClient.report(str);
+
+    System.out.println(msg);
   }
 
-  public long square(int n) {
-    return n * n;
-  }
-
-  public long doubler(int n) {
-    return n + n;
+  public String report(String msg) {
+    return msg + quickStart.square(3);
   }
 }
